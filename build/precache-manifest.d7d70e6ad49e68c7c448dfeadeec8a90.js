1e035f908d6a17d2b042fb5e77b97d707f9c282d
Saniya Kandharkar <saniyakandharkar@gmail.com>
2021-06-30 17:19:26 +0530

    Final Commit

---
+++ build/precache-manifest.d7d70e6ad49e68c7c448dfeadeec8a90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34bacc51c65496549899810a80add95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b89b02c2465c8f8507bc",
+    "url": "/static/css/2.60e43474.chunk.css"
+  },
+  {
+    "revision": "209b7fa55d4786a11a64",
+    "url": "/static/css/main.ad107f83.chunk.css"
+  },
+  {
+    "revision": "b89b02c2465c8f8507bc",
+    "url": "/static/js/2.974a8811.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.974a8811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209b7fa55d4786a11a64",
+    "url": "/static/js/main.c344521d.chunk.js"
+  },
+  {
+    "revision": "def9d47fe2e25b2427de",
+    "url": "/static/js/runtime-main.8bd804d2.js"
+  }
+]);
